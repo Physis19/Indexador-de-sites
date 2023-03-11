b60bb86361312abc6315a3cfa1ec0b2f314b2655
Kauã Fellipe <kauafellipe.bispo@gmail.com>
2023-03-11 11:08:20 -0300

    modified:  <main.py>   reworked menu: add ASCII logo and change options

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from structures import DoublyLinkedList
-
 from rich.console import Console
 from rich.panel import Panel
 from rich import print
@@ -9,12 +8,21 @@ console = Console()
 websites = DoublyLinkedList()
 def menu():
     while True:
-        console.print(Panel("Seja bem vindo ao buscador Oxi ?, escolha alguma das opções abaixo!", title="[bold green]Oxi?[/bold green]"))
-        console.print("1. Adicionar um novo site")
-        console.print("2. Deletar um site")
-        console.print("3. Ver todos os sites")
-        console.print('4. Remover algum site')
-        console.print('0. Sair do programa\n')
+        console.print(Panel.fit('''[bold]
+
+ ________     ___    ___ ___  ________          
+|\   __  \   |\  \  /  /|\  \|\_____  \         
+\ \  \|\  \  \ \  \/  / | \  \|____|\  \        
+ \ \  \\\  \  \ \    / / \ \  \    \ \__\       
+  \ \  \\\  \  /     \/   \ \  \    \|__|       
+   \ \_______\/  /\   \    \ \__\       ___     
+    \|_______/__/ /\ __\    \|__|      |\__\    
+             |__|/ \|__|               \|__|    
+                                                
+ [/bold]                                               
+[bold cyan]1.[/bold cyan] [bold green]Adicionar um novo site [/bold green]          [bold cyan]2.[/bold cyan][bold green] Deletar um site[/bold green]              
+[bold cyan]3.[/bold cyan] [bold green]Ver todos os sites[/bold green]               [bold cyan]0.[/bold cyan] [bold green]Sair do programa[/bold green]
+ ''', title="[bold green]Oxi?[/bold green]"))
         option = console.input('Digite o número da opção desejada: ')
 
         if option == "1":
@@ -22,11 +30,11 @@ def menu():
             websites.append(data)
             console.print(f'O site {data} foi adicionado !')
         elif option == "2":
-            pass
+            data = console.input('Digite a URL do site: ')
+            websites.remove(data)
+            console.print(f'O site {data} removido !')      
         elif option == "3":
             print(websites)
-        elif option == "4":
-            pass
         elif option == "0":
             break
         
