dd4b534fb69aa9a395b9774ff851b0ba2646e603
Kauã Fellipe <kauafellipe.bispo@gmail.com>
2023-03-12 12:35:34 -0300

    modified:   main.py, add menu features, reworked syntax modified:   structures.py add "show_list" fuction

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+import time
+from os import system, name
 from structures import DoublyLinkedList
 from rich.console import Console
 from rich.panel import Panel
-from rich import print
+from rich.table import Table
+from rich import box
+from rich.progress import track
 
 console = Console()
 
 websites = DoublyLinkedList()
+def clear():
+    if name == 'posix':
+        system('clear')
+    else:
+        system('cls')
+
 def menu():
     while True:
         console.print(Panel.fit('''[bold]
@@ -25,18 +35,33 @@ def menu():
  ''', title="[bold green]Oxi?[/bold green]"))
         option = console.input('Digite o número da opção desejada: ')
 
-        if option == "1":
-            data = console.input('Digite a URL do site: ')
-            websites.append(data)
-            console.print(f'O site {data} foi adicionado !')
-        elif option == "2":
-            data = console.input('Digite a URL do site: ')
-            websites.remove(data)
-            console.print(f'O site {data} removido !')      
-        elif option == "3":
-            print(websites)
-        elif option == "0":
-            break
+        match option:
+            case "1":
+                data = console.input('Digite a URL do site: ')
+                websites.append(data)
+                for i in track(range(3), description= 'Adicionando...'):
+                    time.sleep(1)
+                console.print(f'O site {data} foi adicionado !')
+                time.sleep(3)
+                clear()
+            case "2":
+                data = console.input('Digite a URL do site: ')
+                for i in track(range(3), description= 'Removendo...'):
+                    time.sleep(1)
+                websites.remove(data)
+                console.print(f'O site {data} removido !')
+                time.sleep(3)
+                clear()      
+            case "3":
+                websites_table = Table(box = box.ROUNDED, show_lines = True, title = 'Lista de sites :keyboard:')
+                websites_table.add_column("Index", justify="center", style="cyan", no_wrap=True)
+                websites_table.add_column("URL", justify='center', style="magenta")
+                
+                for i in websites.show_list():
+                    websites_table.add_row(f'{i[0]}', f'[link={i[1]}]{i[1]}[/link]')
+                console.print(websites_table)
+            case "0":
+                break
         
 
 menu()
